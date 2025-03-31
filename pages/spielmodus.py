b353fcffa36ea6e9cfd68ac6e0b81a3f9e5c9667
GamblingStar2025 <newspoolestate@gmail.com>
2025-03-31 12:32:30 +0200

    Delete pages/spielmodus.py

--- pages/spielmodus.py
+++
@@ -1,7 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="Spielmodus", layout="wide")
-
-st.title("🎮 Spielmodus auswählen")
-st.write("Wähle hier zwischen Einzelspieler und Gemeinschaftsspiel.")
