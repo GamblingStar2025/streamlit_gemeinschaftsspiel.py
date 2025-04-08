d6b7f32506d3b87796844639020dba5fc81c3bbf
GamblingStar2025 <newspoolestate@gmail.com>
2025-04-08 16:13:27 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="EuroGenius", layout="centered")
-
-st.title("🎯 EuroGenius App")
-st.markdown("Willkommen zur Vorhersage-App für EuroMillions!")
-st.info("Nutze die Navigation links, um zu starten.")
