8ed7020dda36c463559b7ea2c138c180cf1d6091
GamblingStar2025 <newspoolestate@gmail.com>
2025-04-10 01:10:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+
+st.set_page_config(page_title='EuroGenius Start', layout='centered')
+st.title('🎯 Willkommen bei EuroGenius')
+st.write('Nutze die Seitenleiste, um Tipps zu generieren, Strategien zu wählen oder deine Ergebnisse auszuwerten.')
