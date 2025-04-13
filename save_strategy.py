646307133994aa2724e1f9a1f04ce697824f9dc9
GamblingStar2025 <newspoolestate@gmail.com>
2025-04-13 17:39:00 +0200

    Add files via upload

---
+++ save_strategy.py
@@ -0,0 +1,18 @@
+
+import streamlit as st
+from st_supabase_connection import SupabaseConnection
+
+@st.cache_resource
+def get_client():
+    conn = st.connection("supabase", type=SupabaseConnection)
+    return conn.client
+
+def save_strategy(user_email, strategy_name, parameters):
+    supabase = get_client()
+    data = {
+        "email": user_email,
+        "strategy_name": strategy_name,
+        "parameters": parameters
+    }
+    response = supabase.table("strategien").insert(data).execute()
+    return response
